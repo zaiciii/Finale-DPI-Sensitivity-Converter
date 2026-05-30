e2b733d93769c0f6721feff9e5a01fd6877a9b2a
zai <217691155+zaiciii@users.noreply.github.com>
2026-05-30 21:07:52 +0800

    Enhanced GameButton with icon support - ready for game logos

--- src/com/sensitivity/converter/GameButton.java
+++ src/com/sensitivity/converter/GameButton.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.RoundRectangle2D;
+import java.io.File;
 
 public class GameButton extends JButton {
     private boolean isSelected = false;
@@ -13,12 +14,17 @@ public class GameButton extends JButton {
     private Color selectedColor;
     private float cornerRadius = 15f;
     private boolean isHovered = false;
+    private ImageIcon gameIcon;
+    private String iconPath;
 
-    public GameButton(String text) {
+    public GameButton(String text, String gameName) {
         super(text);
         this.baseColor = new Color(60, 63, 81);
         this.hoverColor = new Color(80, 85, 105);
         this.selectedColor = new Color(100, 200, 255);
+        
+        // Try to load game icon from resources/images folder
+        loadGameIcon(gameName);
         
         setOpaque(false);
         setFocusPainted(false);
@@ -41,6 +47,32 @@ public class GameButton extends JButton {
                 repaint();
             }
         });
+    }
+
+    private void loadGameIcon(String gameName) {
+        try {
+            // Try to load from resources/images directory
+            String[] possiblePaths = {
+                "src/resources/images/" + gameName.toLowerCase().replace(" ", "_") + ".png",
+                "resources/images/" + gameName.toLowerCase().replace(" ", "_") + ".png",
+                "images/" + gameName.toLowerCase().replace(" ", "_") + ".png"
+            };
+            
+            for (String path : possiblePaths) {
+                File iconFile = new File(path);
+                if (iconFile.exists()) {
+                    ImageIcon icon = new ImageIcon(path);
+                    // Scale icon to fit button
+                    Image scaledImage = icon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+                    this.gameIcon = new ImageIcon(scaledImage);
+                    this.iconPath = path;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // Icon not found, will display text only
+            this.gameIcon = null;
+        }
     }
 
     @Override
@@ -70,13 +102,20 @@ public class GameButton extends JButton {
         g2d.setStroke(new BasicStroke(2f));
         g2d.draw(roundedRect);
         
+        // Draw icon if available
+        if (gameIcon != null) {
+            int iconX = 10;
+            int iconY = (getHeight() - 30) / 2;
+            gameIcon.paintIcon(this, g2d, iconX, iconY);
+        }
+        
         // Draw text
         g2d.setColor(Color.WHITE);
         g2d.setFont(getFont());
         FontMetrics fm = g2d.getFontMetrics();
-        int x = (getWidth() - fm.stringWidth(getText())) / 2;
-        int y = ((getHeight() - fm.getHeight()) / 2) + fm.getAscent();
-        g2d.drawString(getText(), x, y);
+        int textX = gameIcon != null ? 45 : (getWidth() - fm.stringWidth(getText())) / 2;
+        int textY = ((getHeight() - fm.getHeight()) / 2) + fm.getAscent();
+        g2d.drawString(getText(), textX, textY);
     }
 
     public void setSelected(boolean selected) {
